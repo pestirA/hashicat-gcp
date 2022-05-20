d61b51af648e026929a8d15960be0e9cbfc85d93
pestirA <pauline.abishai@hashicorp.com>
2022-05-20 19:00:21 +0000

    ah ok

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,15 @@ module "network" {
   source  = "terraform-google-modules/network/google"
   version = "5.0.0"
   # insert the 3 required variables here
-    project_id   = var.project
-    network_name = "humble-csa-network"
-    routing_mode = "GLOBAL"
+  project_id   = var.project
+  network_name = "humble-csa-network"
+  routing_mode = "GLOBAL"
+
+  subnets = [
+    {
+      subnet_name   = "humble-csa-subnet"
+      subnet_ip     = "10.100.10.0/24"
+      subnet_region = var.region
+    }
+  ]
 }
